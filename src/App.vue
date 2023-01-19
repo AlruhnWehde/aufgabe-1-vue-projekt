cca81566e2f5966eb446b1819c3348d099875399
AlruhnWehde <alruhn.wehde@rwu.de>
2023-01-19 18:40:31 +0100

    testing deploy

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default{
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! Cool!" />
     </div>
   </header>
 
